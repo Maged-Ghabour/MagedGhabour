e96f4a6b11c6828596dfb788de3fc8fcb5b57265
MagedGhabour <92618945+Maged-Ghabour@users.noreply.github.com>
2023-01-21 20:43:40 +0200

    contact Me

--- js/script.js
+++ js/script.js
@@ -1,38 +1,46 @@
-$(document).ready(function(){
-
-  $('#menu').click(function(){
-    $(this).toggleClass('fa-times');
-    $('header').toggleClass('toggle');
+$(document).ready(function () {
+  $("#menu").click(function () {
+    $(this).toggleClass("fa-times");
+    $("header").toggleClass("toggle");
   });
 
-  $(window).on('scroll load',function(){
+  $(window).on("scroll load", function () {
+    $("#menu").removeClass("fa-times");
+    $("header").removeClass("toggle");
 
-    $('#menu').removeClass('fa-times');
-    $('header').removeClass('toggle');
-
-    if($(window).scrollTop() > 0){
-      $('.top').show();
-    }else{
-      $('.top').hide();
+    if ($(window).scrollTop() > 0) {
+      $(".top").show();
+    } else {
+      $(".top").hide();
     }
-
   });
 
-  // smooth scrolling 
+  // smooth scrolling
 
-  $('a[href*="#"]').on('click',function(e){
-
+  $('a[href*="#"]').on("click", function (e) {
     e.preventDefault();
 
-    $('html, body').animate({
-
-      scrollTop : $($(this).attr('href')).offset().top,
-
-    },
-      500, 
-      'linear'
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top,
+      },
+      500,
+      "linear"
     );
-
   });
+});
 
+// //add active-nav class on click4
+let myNav = document.querySelectorAll("nav ul li");
+let clickSound = document.getElementById("audio4");
+myNav.forEach((ele) => {
+  ele.addEventListener("click", () => {
+    clickSound.play();
+  });
+});
+
+// About click
+let aboutClick = document.getElementById("aboutClick");
+aboutClick.addEventListener("click", () => {
+  document.getElementById("audio1").play();
 });
